b43953b3fe434baf9016598c48ec1481067f7463
yusuke.mizusima <mzsima@gmail.com>
2021-11-10 15:36:29 +0900

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,4 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_hosting_test/main.dart';
 
 void main() {
+  testWidgets('test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('メールアドレス'), findsOneWidget);
+
+  });
 }
